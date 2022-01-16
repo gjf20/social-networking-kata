816e8f32406475153fe577df22ecfb938cedf523
Jamie Flynn <gjf20@case.edu>
2022-01-16 14:10:54 -0700

    Post get time since works for seconds through days

--- post.js
+++ post.js
@@ -1,7 +1,36 @@
+const minuteInSeconds = 60
+const hourInSeconds = minuteInSeconds * 60
+const dayInSeconds = hourInSeconds * 24
+
 class Post {
     constructor(date, message) {
       this.date = date
       this.message = message
+      this.getTimeSince = function() {
+        const currTime = new Date()
+        const diff = currTime.getTime() - this.date.getTime()
+        const secondsSince = diff / 1000
+        var unit 
+        var unitsSince
+        
+        if (secondsSince < minuteInSeconds) {
+          unit = "second"
+          unitsSince = secondsSince
+        } else if (secondsSince < hourInSeconds) {
+          unit = "minute"
+          unitsSince = secondsSince / minuteInSeconds
+        } else if (secondsSince < dayInSeconds){
+          unit = "hour"
+          unitsSince = secondsSince / hourInSeconds
+        } else {
+          unit = "day"
+          unitsSince = secondsSince / dayInSeconds
+        }
+        
+        const singular = unitsSince == 1
+
+        return `(${unitsSince} ${unit}${singular ? "" : "s"} ago)`
+      }
     }
 }
 
--- post.test.js
+++ post.test.js
@@ -13,3 +13,104 @@ describe("Post instance variables", () => {
     expect(subject.message).toBe(message);
   });
 });
+
+describe("Post time since string", () => {
+  test("1 second ago", () => {
+    const expectedMessage = "(1 second ago)"
+    const currentDate = new Date()
+    const millisSince = 1 * 1000
+    const timeDiff = currentDate.getTime() - millisSince
+    const postDate = new Date(timeDiff)
+
+    const subject = new Post(postDate)
+    expect(subject.getTimeSince()).toBe(expectedMessage);
+  });
+  
+  test("10 seconds ago", () => {
+    const expectedMessage = "(10 seconds ago)"
+    const currentDate = new Date()
+    const millisSince = 10 * 1000
+    const timeDiff = currentDate.getTime() - millisSince
+    const postDate = new Date(timeDiff)
+
+    const subject = new Post(postDate)
+    expect(subject.getTimeSince()).toBe(expectedMessage);
+  });
+
+  test("1 minute ago", () => {
+    const expectedMessage = "(1 minute ago)"
+    const currentDate = new Date()
+    const minuteInMillis = 60 * 1000
+    const minuteAgo = currentDate.getTime() - minuteInMillis
+    const postDate = new Date(minuteAgo)
+
+    const subject = new Post(postDate)
+    expect(subject.getTimeSince()).toBe(expectedMessage);
+  });
+
+  test("2 minutes ago", () => {
+    const expectedMessage = "(2 minutes ago)"
+    const currentDate = new Date()
+    const minuteInMillis = 60 * 1000
+    const timeDiff = minuteInMillis * 2
+    const minuteAgo = currentDate.getTime() - timeDiff
+    const postDate = new Date(minuteAgo)
+
+    const subject = new Post(postDate)
+    expect(subject.getTimeSince()).toBe(expectedMessage);
+  });
+
+  test("1 hour ago", () => {
+    const expectedMessage = "(1 hour ago)"
+    const currentDate = new Date()
+    const minuteInMillis = 60 * 1000
+    const hourInMinutes = 60
+    const timeDiff = minuteInMillis * hourInMinutes
+    const minuteAgo = currentDate.getTime() - timeDiff
+    const postDate = new Date(minuteAgo)
+
+    const subject = new Post(postDate)
+    expect(subject.getTimeSince()).toBe(expectedMessage);
+  });
+
+  test("2 hours ago", () => {
+    const expectedMessage = "(2 hours ago)"
+    const currentDate = new Date()
+    const minuteInMillis = 60 * 1000
+    const hourInMinutes = 60
+    const timeDiff = minuteInMillis * hourInMinutes * 2 
+    const minuteAgo = currentDate.getTime() - timeDiff
+    const postDate = new Date(minuteAgo)
+
+    const subject = new Post(postDate)
+    expect(subject.getTimeSince()).toBe(expectedMessage);
+  });
+
+  test("1 day ago", () => {
+    const expectedMessage = "(1 day ago)"
+    const currentDate = new Date()
+    const minuteInMillis = 60 * 1000
+    const hourInMinutes = 60
+    const dayInHours = 24
+    const timeDiff = minuteInMillis * hourInMinutes * dayInHours
+    const minuteAgo = currentDate.getTime() - timeDiff
+    const postDate = new Date(minuteAgo)
+
+    const subject = new Post(postDate)
+    expect(subject.getTimeSince()).toBe(expectedMessage);
+  });
+
+  test("2 days ago", () => {
+    const expectedMessage = "(2 days ago)"
+    const currentDate = new Date()
+    const minuteInMillis = 60 * 1000
+    const hourInMinutes = 60
+    const dayInHours = 24
+    const timeDiff = minuteInMillis * hourInMinutes * dayInHours * 2 
+    const minuteAgo = currentDate.getTime() - timeDiff
+    const postDate = new Date(minuteAgo)
+
+    const subject = new Post(postDate)
+    expect(subject.getTimeSince()).toBe(expectedMessage);
+  });
+});
