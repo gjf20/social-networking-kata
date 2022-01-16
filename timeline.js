1171401fc67c93ae1061cd125ccd96b22c207728
Jamie Flynn <gjf20@case.edu>
2022-01-16 15:05:07 -0700

    Timeline view outputs post

--- timeline.js
+++ timeline.js
@@ -4,7 +4,17 @@ export default class Timeline {
     constructor() {
       this.posts = new List()
       this.view = function () {
-        return "No posts yet"
+        if (this.posts.size == 0) {
+          return "No posts yet"
+        }
+        var feed = ""
+
+        const postArr = this.posts.toArray()
+        postArr.forEach(post => {
+          feed += post.message + " " + post.getTimeSince()
+        });
+        
+        return feed
       }
       this.publish = function(post) {
         this.posts.prepend(post)
